ad4d745e96ba40e1ef6361292019802d1d71183d
costaaaj <miguelcosta220@hotmail.com>
2023-04-25 17:28:55 +0200

    update

--- app/src/main/java/ubiquitous/main/MainActivity.java
+++ app/src/main/java/ubiquitous/main/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
-
     public void goToSignUp(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
